2b445d82848afd9c9bbf0ccd168692432ec3c680
ewvol <bunnieforie@gmail.com>
2026-04-08 12:57:26 -0400

    Made Change Shirt System

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 const outfit = {
     fronthair: "frontbraids.png",
     backhair: "backbraids.png",
-    top: "none", 
-    bottom: "none",
-    shoes: "none",
-    accessories: "none"
+    top: "none.png", 
+    bottom: "none.png",
+    shoes: "none.png",
+    accessories: "none.png"
 }
-const shirts = ["none", "starShirt.png", "longsleeve.png"];
+const shirts = ["none.png", "starShirt.png", "longsleeve.png", "https://placehold.co/100x100/orange/black"];
 let topitem = document.getElementById("top");
 let fronthairitem = document.getElementById("fronthair")
 let backhairitem = document.getElementById("backhair")
@@ -15,15 +15,14 @@ let backhairitem = document.getElementById("backhair")
 
 function wearshirt(item) {
     if (shirts[0]==outfit.top){
-    outfit.top= shirts[item]; 
-    topitem.src = outfit.top;
-    console.log(outfit)
+        outfit.top= shirts[item]; 
     }
     else if (shirts[item]==outfit.top){
         outfit.top= shirts[0]
-        topitem.src=outfit.top
     }
     else{
-        
+        outfit.top=shirts[item]
     }
+    topitem.src = outfit.top;
+    console.log(outfit);
 }
